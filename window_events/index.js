8a021daef45df59ebb0ea5416548a15c20f81bfa
Viktor <palchinskij19@gmail.com>
2024-09-26 12:37:03 +0300

    26.09.24 - window resize event

--- window_events/index.js
+++ window_events/index.js
@@ -1,2 +1,3 @@
 window.addEventListener('load', () => console.log('Page has been loaded.'));
 window.addEventListener('DOMContentLoaded', () => console.log('DOM has been loaded.'));
+window.addEventListener('resize', () => console.log(`Windw's size has been changed: w-${window.innerWidth}, h-${window.innerHeight}`));

69516872de02d85ac3938c4f5d63a71a8f4b93c4
Viktor <palchinskij19@gmail.com>
2024-09-26 13:00:33 +0300

    26.09.24 - window scroll event

--- window_events/index.js
+++ window_events/index.js
@@ -1,3 +1,24 @@
 window.addEventListener('load', () => console.log('Page has been loaded.'));
+
 window.addEventListener('DOMContentLoaded', () => console.log('DOM has been loaded.'));
+
 window.addEventListener('resize', () => console.log(`Windw's size has been changed: w-${window.innerWidth}, h-${window.innerHeight}`));
+
+window.addEventListener('scroll', () => console.log(`Scrilled to ${window.screenY} pixels.`));
+
+// window.addEventListener('beforeunload', (event) => {
+//     event.preventDefault();
+//     console.log('beforeunload');
+// });
+
+// window.addEventListener('unload', () => console.log('Page has been unloaded.'));
+
+// window.addEventListener('focus', () => console.log('Page is active.'));
+
+// window.addEventListener('blur', () => console.log('Page is passive.'));
+
+// window.addEventListener('DOMContentLoaded', () => console.log('DOM has been loaded.'));
+
+// window.addEventListener('DOMContentLoaded', () => console.log('DOM has been loaded.'));
+
+// window.addEventListener('DOMContentLoaded', () => console.log('DOM has been loaded.'));

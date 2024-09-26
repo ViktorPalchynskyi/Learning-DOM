ef726f89c6dff42a87e73146e842053f3be821cf
Viktor <palchinskij19@gmail.com>
2024-09-26 13:26:53 +0300

    26.09.24 - onload and onerror file events during downloading

--- window_events/index.js
+++ window_events/index.js
@@ -17,8 +17,27 @@ window.addEventListener('focus', () => console.log('Page is active.'));
 
 window.addEventListener('blur', () => console.log('Page is passive.'));
 
-window.addEventListener('error', (event) => console.log(`Error: ${event.message} on ${event.filename} in line ${event.lineno}.`));
+window.addEventListener('error', (event) => console.log(`Error: ${event.message} on ${event.filename} in line ${event.lineno}:${event.colno}.`));
 
 window.addEventListener('online', () => console.log('Connection restored.'));
 
 window.addEventListener('offline', () => console.log('Connection was been lost.'));
+
+const script = document.createElement('script');
+
+script.src = './test.js';
+
+document.head.append(script);
+
+script.onload = function () {
+    console.log(TEST);
+    console.log(this);
+};
+
+let script2 = document.createElement('script');
+script2.src = 'https://example.com/404.js'; 
+document.head.append(script2);
+
+script2.onerror = function () {
+    console.log(`Download error ${this.src}`); 
+};
